1304aa645ced9ad51bfab303c000a6a09a150140
mansi <mansijajadiya@gmail.com>
2026-08-18 16:35:41 +0530

    feat: initial code

---
+++ index.js
@@ -0,0 +1,17 @@
+// Variables
+
+// let
+    // let can be reassigned but cannot be redeclared
+    let firstName = 'Pratik' // = is an assignment operator
+    firstName = 'John'
+    console.log(firstName);
+
+// var
+    // let can be reassigned AS WELL AS redeclared
+    var a = 'five'
+    var a = 'six'
+    console.log(a);
+
+// const 
+    // const cannot be reassigned nor redeclared
+    const c = 'one'
