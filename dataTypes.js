3e0cc0591436dca71b53ad0cca80b044dbf88251
Pratik <pratik080.rejoice@gmail.com>
2026-08-24 17:07:22 +0530

    add: methods

--- dataTypes.js
+++ dataTypes.js
@@ -9,7 +9,7 @@
         // console.log(typeof(Number('55') + Boolean(' ')))
 
         const lengthyStr = '     why so serious?     '
-        console.log(lengthyStr.length);
+        // console.log(lengthyStr.length);
         // console.log(lengthyStr.charAt(14));
         // console.log(lengthyStr.charCodeAt(14));
         // console.log(lengthyStr.concat(' ', 'testing', ' ', 'working'));
@@ -26,27 +26,39 @@
         // console.log(lengthyStr.trimEnd());
         // console.log(lengthyStr.padEnd(50, '*'));
         // console.log(lengthyStr.padStart(50, '*'));
-            
+        // console.log(lengthyStr.replace('s', '%'));
+        // console.log(lengthyStr.replaceAll('s', '%'));
+        // console.log(lengthyStr.split('*'));
         
+        // search methods
+        // console.log(lengthyStr.indexOf('s'));
+        // console.log(lengthyStr.lastIndexOf('*'));
+        // console.log(lengthyStr.includes('so'));
+        // console.log(lengthyStr.startsWith(' '));
+        // console.log(lengthyStr.endsWith(' '));
         
     // Number
         // define - 123, 34.5, -234, 0 - -2^53 to 2^53
         // console.log(typeof String(33));
-        
+        const numm = 234234.566456
+        // console.log(numm.toFixed(1));
+        // console.log(numm.toExponential(3));
 
     // Boolean
         // true, false
         // console.log(Boolean(null))
+
     // null
         // const fistName = null
+
     // undefined
         // undefined
+
     // BigInt
         // 1n
+
     // Symbol
         // Symbol()
-
-
 
 
 // Non-premitive / Complex
@@ -54,6 +66,12 @@
         // define - { key: value, key: value }
         // console.log('User', { name: 'John', age: 30 });
         // console.log('Laptop', { screen: '13 inch', ram: '16 gb', ssd: 512 });
+        const base = {id: 33443523432, name: 'stella'}
+        const objj = Object.assign(base, { name: 'john'}, {age: 30, hobby: ['biking', 'reading']})
+        // console.log(objj)
+        // console.log(Object.keys(objj))
+        // console.log(Object.values(objj))
+        // console.log(Object.entries(objj))
         
     // Array
         // define - []
